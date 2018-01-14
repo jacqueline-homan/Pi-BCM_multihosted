a608a655b24ff8d597703f4e0f4722970df85269
Alexei Bushuev <alexeib.2018@gmail.com>
2018-01-14 10:02:56 +0300

    adapted AccountCreateOrUpdate for django

--- gs1ie/views.py
+++ gs1ie/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+class User:
+    def is_authenticated(self):
+        return True
+
+
+def account_create_or_update(request):
+    current_user = User()
+    context = { 'current_user' : current_user,
+                'active_page' : '', }
+    return render(request, 'gs1ie/AccountCreateOrUpdate.html', context)
+

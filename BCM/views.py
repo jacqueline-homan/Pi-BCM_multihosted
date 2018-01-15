0edd29b462023fd5b2f81f278912383972b8f759
Hikaon <hikaon.ur@gmx.com>
2018-01-15 17:01:01 +0100

    Some cleanup

--- BCM/views.py
+++ BCM/views.py
@@ -4,13 +4,17 @@ import django.utils.translation as trans
 # Create your views here.
 
 
+def set_language(lang, request):
+    trans.activate(lang)
+    request.session[trans.LANGUAGE_SESSION_KEY] = lang
+
+
 def index_redirect(request):
     if request.method == "POST":
         country = request.POST.get("country")
         country_obj = models.Country.objects.filter(slug=country).first()
         lang = country_obj.get_default_language()
-        trans.activate(lang.language.slug)
-        request.session[trans.LANGUAGE_SESSION_KEY] = lang.language.slug
+        set_language(lang.language.slug, request)
         return redirect(reverse('index', args=[country]))
     else:
         countries = models.Country.objects.all()
@@ -20,6 +24,8 @@ def index_redirect(request):
 def index(request, country):
     langs = models.LanguageByCountry.objects.filter(country__slug=country)
     if request.method == "POST":
-        default_lang = models.LanguageByCountry.objects.filter(country__slug=country, default=True)
-        selected_lang = request.POST.get("language", default_lang)
+        default_lang = models.LanguageByCountry.objects.filter(
+            country__slug=country, default=True).first()
+        selected_lang = request.POST.get("language", default_lang.language.slug)
+        set_language(selected_lang, request)
     return render(request, 'index.html', {"languages": langs})

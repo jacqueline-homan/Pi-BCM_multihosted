1efa6aa914884cee157dc10fc1e2b58f07b7ee87
Alexei Bushuev <alexeib.2018@gmail.com>
2018-01-22 15:37:28 +0300

    developed Prefixes form validation

--- prefixes/views.py
+++ prefixes/views.py
@@ -1,9 +1,12 @@
+from django.http import Http404
 from django.shortcuts import render
 from django.conf import settings
 from services import prefix_service, users_service
+from .forms import PrefixActionForm
 
 
 def prefixes(request):
+    flashed_messages = []
     current_user = {
         'id': 1,
         'active': True,
@@ -23,8 +26,6 @@ def prefixes(request):
     susp_prefixes = prefix_service.find(organisation=current_user['organisation'], is_suspended=True).all()
 
     '''
-    susp_prefixes = prefix_service.find(organisation=current_user.organisation, is_suspended=True).all()
-
     prefixes = prefix_service.find_all().all()
     result = db.session.query('prefix', 'products'). \
         from_statement(text("select products.gs1_company_prefix as prefix, 
@@ -53,12 +54,100 @@ def prefixes(request):
                 setattr(prefix, 'locations', row[1])
     '''
 
+    if request.method == 'POST':
+        form = PrefixActionForm(request.POST)
+        form.fields['select_prefix'].choices = [(str(p.id), p.prefix) for p in prefixes]
+        if form.is_valid():
+            try:
+                int_prefix_id = int(form.data['select_prefix'])
+            except (ValueError, TypeError):
+                flashed_messages.append(('Your selections were not valid!', 'danger'),)
+            else:
+                prefix = prefix_service.get(int_prefix_id)
+                if not prefix:
+                    raise Http404('Prefix not found')
+
+                if prefix.is_special == 'READ-ONLY' and form.data['prefix_action'] != 'set_this':
+                    flashed_messages.append(('Read-only prefix, please contact GS1 helpdesk.', 'danger'),)
+                else:
+                    # prefix_service.make_active(prefix.id)
+
+                    if form.data['prefix_action'] == 'set_this':
+                        pass
+                        # return redirect(url_for('products.products_list'))
+
+                    elif form.data['prefix_action'] == 'first_available':
+                        pass
+                        '''
+                        try:
+                            prefix.make_starting_from()
+                        except:
+                            return render_template('prefixes/prefix_exhausted.html', prefix=prefix)
+                        prefix_service.save(prefix)
+                        return redirect(url_for('.prefixes_list'))
+                        '''
+
+                    elif form.data['prefix_action'] == 'first_available_gln':
+                        pass
+                        '''
+                        try:
+                            prefix.make_starting_from_gln()
+                        except Exception,e:
+                            return render_template('prefixes/prefix_exhausted.html', prefix=prefix)
+                        prefix_service.save(prefix)
+                        return redirect(url_for('.prefixes_list'))
+                        '''
+
+                    elif form.data['prefix_action'] == 'starting_gtin':
+                        pass
+                        #return redirect(url_for('.prefixes_set_starting', prefix_id=str(prefix.id)))
+
+                    # new product
+                    elif form.data['prefix_action'] == 'new_product':
+                        pass
+                        #return redirect(url_for('products.add_product') + '?prefix=' + str(prefix.prefix))
+
+                    # new location
+                    elif form.data['prefix_action'] == 'new_gln':
+                        pass
+                        #return redirect(url_for('locations.add_location') + '?prefix=' + str(prefix.prefix))
+
+                    elif form.data['prefix_action'] == 'export_available':
+                        pass
+                        '''
+                        try:
+                            products = Product.query.filter(Product.owner == current_user,
+                                                            Product.gs1_company_prefix == prefix.prefix) \
+                                .order_by(Product.gtin).all()
+                            prfxs = prefix.get_available_gtins(products)
+                            if len(prfxs) > 0:
+                                tfile = tempfile.NamedTemporaryFile(suffix='.xlsx')
+                                zfile = tempfile.NamedTemporaryFile(suffix='.zip')
+                                file_xlsx = load_workbook(os.path.join(EXCEL_TEMPLATE_PATH, GDSN_EXCEL_TEMPLATE))
+                                ws = file_xlsx.get_active_sheet()
+                                for index, prfx in enumerate(prfxs):
+                                    _ = ws.cell(column=2, row=index + 5, value=prfx)
+                                file_xlsx.save(filename=tfile.name)
+                                with ZipFile(zfile.name, "w") as z:
+                                    export_filename = "export_%s_available.xlsx" % (prefix.prefix,)
+                                    attachment_filename = "export_%s_available.%s" % (prefix.prefix, 'zip')
+                                    z.write(tfile.name, export_filename)
+                                return send_file(zfile, mimetype='application/zip',
+                                                 as_attachment=True,
+                                                 attachment_filename=attachment_filename)
+                            else:
+                                flash("There are no available GTIN numbers for current active prefix", "danger")
+                        '''
+        else:
+            flashed_messages.append(('You must choose a prefix and an action!', 'danger'),)
+
     config = {'GS1_GLN_CAPABILITY': settings.GS1_GLN_CAPABILITY}
 
     context = {
         'current_user': current_user,
         'config': config,
         'prefixes': prefixes,
-        'susp_prefixes': susp_prefixes
+        'susp_prefixes': susp_prefixes,
+        'flashed_messages': flashed_messages
     }
     return render(request, 'prefixes/prefixes_list.html', context)

8c60c7331c96e1b5951acd049c02a0f623cf5097
Alexei Bushuev <alexeib.2018@gmail.com>
2018-02-01 13:06:59 +0300

    load fixtures: python manage.py loaddata fixtures/*

--- products/models/package_level.py
+++ products/models/package_level.py
@@ -3,8 +3,7 @@ from django.db import models
 
 
 class PackageLevel(models.Model):
-    db_name = 'package_level'
-
+    
     # Package level description
     level = models.CharField(max_length=100, default='')
     unit_descriptor = models.CharField(max_length=50)
